30a8607a7b3c6a5b66ce54e30b1b4da279175af1
EstebanFerreyra <eferreyra098@gmail.com>
2023-08-15 12:39:39 -0300

    Otra manera de resolver helloworld
    Esta manera de resolver permite que el programa salude en distitnos idiomas de acuerdo a lo que ingresa el usuario.

--- LabNet2023.HelloWorld/LabNet2023.HelloWorld/Program.cs
+++ LabNet2023.HelloWorld/LabNet2023.HelloWorld/Program.cs
@@ -8,9 +8,60 @@ namespace LabNet2023.HelloWorld
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World");
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorld");
+            Console.WriteLine("SALUDO (i - ingles | e - español | 0 - salir)");
+            string letter;
+
+            do
+            {
+                letter = LetterValidation();
+
+                Console.WriteLine(Greeting(letter));
+
+                if (letter != "0")
+                {
+                    Console.WriteLine("SALUDO (i - ingles | e - español | 0 - salir)");
+                }
+            } while (letter != "0");
+            Console.ReadKey();
+        }
+
+        static string LetterValidation()
+        {
+            string letter = Console.ReadLine();
+
+            while ((letter.Length != 0) && (letter != "i") && (letter != "e") && (letter != "0"))
+            {
+                Console.WriteLine("Porfavor ingrese una opción válida: ");
+                letter = Console.ReadLine();
+            }
+            return letter;
+        }
+
+        static string Greeting(string letter)
+        {
+            const string greetingInSpanish = "Hola mundo!";
+            const string greetingInEnglish = "Hello world!";
+            const string farewell = "Hasta la proxima!";
+
+            if (letter != "e")
+            {
+                if (letter == "i")
+                {
+                    return greetingInEnglish;
+                }
+            }
+            else
+            {
+                return greetingInSpanish;
+            }
+            return farewell;
         }
     }
 }
